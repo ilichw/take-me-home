aacf4db3c250906b19406745610ffa8b9a7d5522
Ivan <ilichev-ivn@yandex.ru>
2025-11-18 18:04:31 +0300

    initial commit

---
+++ src/scripts/game.js
@@ -0,0 +1,28 @@
+import Phaser from 'phaser';
+import './style.css';
+
+const config = {
+  type: Phaser.CANVAS,
+  width: 800,
+  height: 600,
+  id: 'game',
+  scene: {
+    preload: preload,
+    create: create,
+    update: update
+  }
+};
+
+const game = new Phaser.Game(config);
+
+function preload() {
+  // Загрузка активов
+}
+
+function create() {
+  // Инициализация игры
+}
+
+function update() {
+  // Логика обновления игры
+}
